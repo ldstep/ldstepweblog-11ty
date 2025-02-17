e63ff52d6c3d632d072ca9a5ecb608fc8c3b4201
Loren Stephens <ldstep@ldstephens.com>
2025-02-17 13:16:25 -0500

    Update rss plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { DateTime } = require("luxon");
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss"); // Add this line
 
 module.exports = function (eleventyConfig) {
    eleventyConfig.setTemplateFormats([
@@ -15,26 +16,20 @@ module.exports = function (eleventyConfig) {
       "woff",
       "woff2",
    ]);
-
    // Add passthrough copy for CSS directory
    eleventyConfig.addPassthroughCopy("./src/css/");
-
    // Handle the style.css file in public directory
    eleventyConfig.addPassthroughCopy({ "src/public/style.css": "style.css" });
-
    // Add watch target for CSS
    eleventyConfig.addWatchTarget("./src/css/");
    eleventyConfig.addWatchTarget("./src/public/style.css");
-
    // Filters let you modify the content https://www.11ty.dev/docs/filters/
    eleventyConfig.addFilter("htmlDateString", (dateObj) => {
       return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
          "yyyy-LL-dd"
       );
    });
-
    eleventyConfig.setBrowserSyncConfig({ ghostMode: false });
-
    /* Build the collection of posts to list in the site
     * - Read the Next Steps post to learn how to extend this
     */
@@ -44,7 +39,6 @@ module.exports = function (eleventyConfig) {
        */
       // Get filtered collection and reverse chronological order
       const coll = collection.getFilteredByTag("posts").reverse();
-
       // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426
       // Adds {{ prevPost.url }} {{ prevPost.data.title }}, etc, to our njks templates
       for (let i = 0; i < coll.length; i++) {
@@ -53,8 +47,27 @@ module.exports = function (eleventyConfig) {
          coll[i].data["prevPost"] = prevPost;
          coll[i].data["nextPost"] = nextPost;
       }
-
       return coll;
+   });
+
+   // Add the RSS plugin configuration here
+   eleventyConfig.addPlugin(feedPlugin, {
+      type: "atom", // or "rss", "json"
+      outputPath: "/feed.xml",
+      collection: {
+         name: "posts", // This matches your existing posts collection
+         limit: 10, // 0 means no limit
+      },
+      metadata: {
+         language: "en",
+         title: "lstep.xyz", // Change this to your site's title
+         subtitle: "A blog by Loren.", // Change this
+         base: "https://lstep.xyz", // Change to your actual domain
+         author: {
+            name: "Loren", // Change to your name
+            email: "", // Optional - add your email if you want
+         },
+      },
    });
 
    return {
