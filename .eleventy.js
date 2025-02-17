d9e24a4669e3236734f18faca2e1f47a0a00059a
Loren Stephens <ldstep@ldstephens.com>
2025-02-17 10:51:38 -0500

    Updated
    Made changes to manually add rss feed.

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,12 @@
 const { DateTime } = require("luxon");
-const rssPlugin = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
-   // Add RSS Plugin
-   eleventyConfig.addPlugin(rssPlugin);
-
-   // Custom filter to limit array length
-   eleventyConfig.addFilter("limit", function (array, limit) {
-      return array.slice(0, limit);
-   });
-
-   // Set Template Formats
    eleventyConfig.setTemplateFormats([
+      // Templates:
       "html",
       "njk",
       "md",
+      // Static Assets:
       "css",
       "jpeg",
       "jpg",
@@ -24,29 +16,37 @@ module.exports = function (eleventyConfig) {
       "woff2",
    ]);
 
-   // Add passthrough copy for CSS
+   // Add passthrough copy for CSS directory
    eleventyConfig.addPassthroughCopy("./src/css/");
+
+   // Handle the style.css file in public directory
    eleventyConfig.addPassthroughCopy({ "src/public/style.css": "style.css" });
 
-   // Add watch targets
+   // Add watch target for CSS
    eleventyConfig.addWatchTarget("./src/css/");
    eleventyConfig.addWatchTarget("./src/public/style.css");
 
-   // Date filter
+   // Filters let you modify the content https://www.11ty.dev/docs/filters/
    eleventyConfig.addFilter("htmlDateString", (dateObj) => {
       return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
          "yyyy-LL-dd"
       );
    });
 
-   // Configure BrowserSync
    eleventyConfig.setBrowserSyncConfig({ ghostMode: false });
 
-   // Build post collection
+   /* Build the collection of posts to list in the site
+    * - Read the Next Steps post to learn how to extend this
+    */
    eleventyConfig.addCollection("posts", function (collection) {
+      /* The posts collection includes all posts that list 'posts' in the front matter 'tags'
+       * - https://www.11ty.dev/docs/collections/
+       */
+      // Get filtered collection and reverse chronological order
       const coll = collection.getFilteredByTag("posts").reverse();
 
-      // Add previous and next post references
+      // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426
+      // Adds {{ prevPost.url }} {{ prevPost.data.title }}, etc, to our njks templates
       for (let i = 0; i < coll.length; i++) {
          const prevPost = coll[i - 1];
          const nextPost = coll[i + 1];
